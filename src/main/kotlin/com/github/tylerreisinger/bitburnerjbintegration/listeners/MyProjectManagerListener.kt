231879b9cb442c27b26e5aea644ff5eac46d3449
GitHub Action <action@github.com>
2023-03-02 13:51:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/tylerreisinger/bitburnerjbintegration/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tylerreisinger/bitburnerjbintegration/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tylerreisinger.bitburnerjbintegration.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tylerreisinger.bitburnerjbintegration.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
